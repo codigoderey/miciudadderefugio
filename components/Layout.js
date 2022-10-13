fe98fbf426c09b8e6f9fcc8342759fdbe2cded8a
Reynaldo Navedo <codigoderey@outlook.com>
2022-10-12 22:35:49 -0400

    minor changes

--- components/Layout.js
+++ components/Layout.js
@@ -1,33 +1,61 @@
-import React from 'react';
-import Head from 'next/head';
+import React from "react";
+import Head from "next/head";
 
 const Layout = ({ pageTitle, children }) => {
-  return (
-    <div>
-      <Head>
-        <title>{pageTitle}</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/favicons/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicons/favicon-16x16.png" />
-        <link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.min.css" />
-        <link rel="stylesheet" type="text/css" href="/assets/css/animate.min.css" />
-        <link rel="stylesheet" type="text/css" href="/assets/css/fontawesome-all.min.css" />
-        <link rel="stylesheet" type="text/css" href="/assets/css/zimed-icon.css" />
-        <link rel="stylesheet" type="text/css" href="/assets/css/style.css" />
-        <link rel="stylesheet" type="text/css" href="/assets/css/responsive.css" />
-        <link rel="stylesheet" type="text/css" href="/assets/css/spinner.css" />
+	return (
+		<div>
+			<Head>
+				<title>{pageTitle}</title>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/assets/images/favicons/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/assets/images/favicons/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/assets/images/favicons/favicon-16x16.png"
+				/>
+				<link
+					rel="stylesheet"
+					type="text/css"
+					href="/assets/css/animate.min.css"
+				/>
+				<link
+					rel="stylesheet"
+					type="text/css"
+					href="/assets/css/fontawesome-all.min.css"
+				/>
+				<link
+					rel="stylesheet"
+					type="text/css"
+					href="/assets/css/zimed-icon.css"
+				/>
+				<link rel="stylesheet" type="text/css" href="/assets/css/style.css" />
+				<link
+					rel="stylesheet"
+					type="text/css"
+					href="/assets/css/responsive.css"
+				/>
+				<link rel="stylesheet" type="text/css" href="/assets/css/spinner.css" />
+				<link
+					rel="stylesheet"
+					type="text/css"
+					href="/assets/css/bootstrap.min.css"
+				/>
+			</Head>
 
-      </Head>
-
-      <div className="page-wrapper">
-
-        {children}
-
-      </div>
-
-    </div>
-  )
-}
+			<div className="page-wrapper">{children}</div>
+		</div>
+	);
+};
 export default Layout;
--- pages/api/eventos.js
+++ pages/api/eventos.js
@@ -1,106 +1,121 @@
-import Evento from "../../models/Eventos"
-import Cors from "cors"
-import connectDB from "../../utils/connectDB"
+import Evento from "../../models/Eventos";
+import Cors from "cors";
+import connectDB from "../../utils/connectDB";
 
-connectDB()
+connectDB();
 
 // Initializing the cors middleware
 const cors = Cors({
-  methods: ['GET', 'HEAD'],
+	methods: ["GET", "HEAD"]
 });
 
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
 function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
+	return new Promise((resolve, reject) => {
+		fn(req, res, (result) => {
+			if (result instanceof Error) {
+				return reject(result);
+			}
 
-      return resolve(result);
-    });
-  });
+			return resolve(result);
+		});
+	});
 }
 
 // switch functions method conditionals
 export default (req, res) => {
-  switch (req.method) {
-    case "GET":
-      handleGetRequest(req, res)
-      break;
+	switch (req.method) {
+		case "GET":
+			handleGetRequest(req, res);
+			break;
 
-    case "POST":
-      handlePostRequest(req, res)
-      break;
+		case "POST":
+			handlePostRequest(req, res);
+			break;
 
-    case "PUT":
-      handlePutRequest(req, res)
-      break;
+		case "PUT":
+			handlePutRequest(req, res);
+			break;
 
-    case "DELETE":
-      handleDeleteRequest(req, res)
-      break;
+		case "DELETE":
+			handleDeleteRequest(req, res);
+			break;
 
-    default:
-      res.status(404).send("El método utilizado no funcionó. Intente nuevamente.")
-  }
-}
+		default:
+			res
+				.status(404)
+				.send("El método utilizado no funcionó. Intente nuevamente.");
+	}
+};
 
 const handlePostRequest = async (req, res) => {
+	runMiddleware(req, res, cors);
 
-  runMiddleware(req, res, cors)
+	try {
+		const {
+			nombreDelEvento,
+			organizadoPor,
+			lugar,
+			descripcionDelEvento,
+			direccionCalle,
+			direccionCiudad,
+			direccionEstado,
+			direccionZipCode,
+			cantidadDisponible,
+			imageUrl,
+			fechasYHorasDelEvento
+		} = req.body;
 
-  try {
+		const nuevoEvento = await new Evento({
+			nombreDelEvento,
+			organizadoPor,
+			lugar,
+			descripcionDelEvento,
+			direccionCalle,
+			direccionCiudad,
+			direccionEstado,
+			direccionZipCode,
+			cantidadDisponible,
+			imageUrl,
+			fechasYHorasDelEvento
+		});
 
-    const { nombreDelEvento, organizadoPor, lugar, descripcionDelEvento, direccionCalle, direccionCiudad, direccionEstado, direccionZipCode, cantidadDisponible, imageUrl, fechasYHorasDelEvento } = req.body
-
-    const nuevoEvento = await new Evento({
-      nombreDelEvento,
-      organizadoPor,
-      lugar,
-      descripcionDelEvento,
-      direccionCalle,
-      direccionCiudad,
-      direccionEstado,
-      direccionZipCode,
-      cantidadDisponible,
-      imageUrl,
-      fechasYHorasDelEvento
-    })
-
-    await nuevoEvento.save()
-    res.status(200).send("El nuevo evento ha sido añadido.")
-  } catch (error) {
-    console.error(error)
-    res.status(403).send("Hubo un error al crear el evento, verifica que todos los blancos estén completados.")
-  }
-}
+		await nuevoEvento.save();
+		res.status(200).send("El nuevo evento ha sido añadido.");
+	} catch (error) {
+		console.error(error);
+		res
+			.status(403)
+			.send(
+				"Hubo un error al crear el evento, verifica que todos los blancos estén completados."
+			);
+	}
+};
 
 const handleGetRequest = async (req, res) => {
+	runMiddleware(req, res, cors);
 
-  runMiddleware(req, res, cors)
-
-  try {
-    const eventos = await Evento.find()
-    res.status(200).send(eventos)
-  } catch (error) {
-    console.log(error)
-    res.status("Hubo un error, intenta nuevamente.")
-  }
-}
+	try {
+		const eventos = await Evento.find();
+		res.status(200).send(eventos);
+	} catch (error) {
+		console.log("Error: " + error.message);
+		res.status("Hubo un error, intenta nuevamente.");
+	}
+};
 
 const handleDeleteRequest = async (req, res) => {
-  runMiddleware(req, res, cors)
+	runMiddleware(req, res, cors);
 
-  try {
-    const { id } = req.query
+	try {
+		const { id } = req.query;
 
-    await Evento.findOneAndDelete({ _id: id })
+		await Evento.findOneAndDelete({ _id: id });
 
-    res.send("El evento a sido eliminado exitosamente")
-  } catch (error) {
-    console.error(error)
-    res.status(500).send("Hubo un error, intente nuevamente.")
-  }
-}
+		res.send("El evento a sido eliminado exitosamente");
+	} catch (error) {
+		console.log("Error: " + error.message);
+		res.status(500).send("Hubo un error, intente nuevamente.");
+	}
+};
